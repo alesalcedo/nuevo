ecf1065cf1d704151d4b52b2e8427dd09e1d99bd
Alejandro Salcedo <alesalcedo@gmail.com>
2023-01-20 17:50:22 -0300

    proyecto_nuevo

---
+++ src/componentes/Navbar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import ShoppingCartOutlined from '@material-ui/icons/ShoppingCart';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import logo from './images/logo-zapa.png';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(50),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root} >
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <img src={logo} alt="Logo" height="150" width="150" />
+          </IconButton>
+          <Button color="inherit" href="/">Inicio</Button>
+          <Button color="inherit" href="/">Productos</Button>
+          <Button color="inherit" href="/">Hombre</Button>
+          <Button color="inherit" href="/">Mujer</Button>
+          <Button color="inherit" href="/">Niño</Button>
+          <IconButton color="inherit" aria-label="carrito"></IconButton>
+            <ShoppingCartOutlined/>
+            <IconButton  color="inherit" aria-label="buscar"> </IconButton>
+            <SearchOutlinedIcon fontSize="large" />
+            <IconButton color="inherit" aria-label="ingresar"></IconButton>
+            <AccountCircleOutlinedIcon/>
+
+          </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+export default Navbar
---
+++ src/componentes/Producto.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';  
+import { AddShoppingCart} from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  action:{
+    marginTop: '3 rem',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+
+}));
+
+function Producto() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+       
+        
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        className={classes.media}
+        image=''
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Zapatillas
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to Cart" >
+            <AddShoppingCart fontSize='large'/>
+        
+        </IconButton>
+        {Array(4)
+        .fill()
+        .map((_,i) => (
+            <p>&#11088;</p>
+        ))}
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          
+
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
+export default Producto
